529c0274ae6bb807bd0deb7202499ce81aa7a3e5
Thuytungoc <ngocthuydhnl@gmail.com>
2025-06-11 16:07:16 +0700

    Add files via upload

---
+++ wallet.h
@@ -0,0 +1,15 @@
+//#pragma once
+#include <string>
+#include <vector>
+class Wallet {
+public:
+    std::string wallet_id, username;
+    int balance;
+    Wallet();
+    Wallet(const std::string&, const std::string&, int);
+    static void loadWallets(std::vector<Wallet>&);
+    static void saveWallets(const std::vector<Wallet>&);
+    static Wallet* findWalletByUsername(std::vector<Wallet>&, const std::string&);
+    static Wallet* findWalletById(std::vector<Wallet>&, const std::string&);
+    static void ensureTotalWallet(std::vector<Wallet>&);
+};
